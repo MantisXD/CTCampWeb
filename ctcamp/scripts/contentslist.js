7faba38aa6ac6aad616e22668a2b35c9e2978ca8
MantisXD <mantis@mantisxp.xyz>
2017-12-10 05:03:19 +0900

    main page css

---
+++ ctcamp/scripts/contentslist.js
@@ -0,0 +1,55 @@
+if (typeof g_sessionSrcList == "undefined") g_contentsList = [];
+g_contentsList.push(
+{	
+	"contents":[  
+	    {  
+	        "conference":"ctcamp1",
+	        "id":"ctcamp1_01",
+	        "title":"ct camp web",
+	        "subtitle":"web good",
+	        "presenter":"mantis1",
+	        "description":"ctcamp \uc6f9\uc744 \ub9cc\ub4e0 \ud504\ub85c\uc81d\ud2b8 \uc785\ub2c8\ub2e4.\n \uadf8\ub807\uc2b5\ub2c8\ub2e4 \uc774\ub294 \uc5ec\ub7ec \uc904\uc774 \uac00\ub2a5\ud55c\uc9c0 \uccb4\ud06c \ud558\uae30 \uc704\ud574 \uc791\uc131 \ub41c \uc904\uc785\ub2c8\ub2e4."
+	    },
+	    {  
+	        "conference":"ctcamp3",
+	        "id":"ctcamp3_01",
+	        "title":"ct camp ai",
+	        "subtitle":"",
+	        "presenter":"mantis3",
+	        "description":"\u3141\u3134\u3147\u3139\u3134\u3147\u3139"
+	    },
+	    {  
+	        "conference":"ctcamp2",
+	        "id":"ctcamp2_01",
+	        "title":"ct camp algorithm",
+	        "subtitle":"",
+	        "presenter":"mantis1",
+	        "description":"\u3134\u3147\u3139\u3141\u3147\u3134\u3139\u3134\u3147\u3139\u3141\u3147\u3134\u3139\n \u3141\u3134\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139 \n \u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147 \n \u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139 \n \u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3139"
+	    },
+	    {  
+	        "conference":"ctcamp1",
+	        "id":"ctcamp1_02",
+	        "title":"turing machine",
+	        "subtitle":"",
+	        "presenter":"mantisxp",
+	        "description":"\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139"
+	    },
+	    {  
+	        "conference":"ctcamp2",
+	        "id":"ctcamp2_02",
+	        "title":"postech mystery jerney",
+	        "subtitle":"",
+	        "presenter":"mantisxd",
+	        "description":"\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139"
+	    },
+	    {  
+	        "conference":"ctcamp1",
+	        "id":"ctcamp1_01",
+	        "title":"fun coding",
+	        "subtitle":"coding is fun",
+	        "presenter":"mantis\u3141",
+	        "description":"\u3141\u3134\u3147\u3139\u3141\u3134\u3147 \n \u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139 \n \u3141\u3134\u3147\u3139\u3141\u3134\u3147\u3139"
+	    }
+	]
+}
+);
--- xls2json/run.py
+++ xls2json/run.py
@@ -1,10 +1,11 @@
+#!/usr/bin/env python
 import csv
 import json
 
 csvfile = open('contents.csv', 'r')
 jsonfile = open('contents.json', 'w')
 
-fieldnames = ("CT Camp 기수","ID","제목","부제","담당자","설명")
+fieldnames = ("ctcampnum","id","tilte","subtitle","manager","description")
 reader = csv.DictReader( csvfile, fieldnames)
 for row in reader:
   json.dump(row, jsonfile)
